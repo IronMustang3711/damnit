d3ebe2faf599b725df38b23ba37a91b7849023bc
jasonnn <jasonmarkham@gmail.com>
2018-02-15 14:36:44 -0800

    split up positions.h/cpp

--- Damnit/src/auto/positions.h
+++ Damnit/src/auto/positions.h
@@ -25,121 +25,65 @@
  */
 
 
-
 enum class StartPosition {
-
-    TOP,MIDDLE,BOTTOM
+    TOP,
+    MIDDLE,
+    BOTTOM
 };
-enum class Target {
-    SWITCH=0,
-    SCALE,
-    OPPOSING_SWITCH
+
+enum class AutoTarget {
+    NONE = 0,
+    LINE,
+    SWITCH,
+    SCALE
 };
 enum class TargetLocation {
     INVALID,
     TOP = 1,
     BOTTOM,
+
     LEFT = static_cast<std::underlying_type_t<TargetLocation>>(TargetLocation::TOP),
     RIGHT = static_cast<std::underlying_type_t<TargetLocation>>(TargetLocation::BOTTOM)
 };
 
 enum class Alliance {
-   // INVALID = 0,
-    RED,BLUE
+    RED,
+    BLUE
 };
 
-TargetLocation parse_TargetLocation(char c){
-    switch(c){
-        case 'L' : return TargetLocation::LEFT;
-        case 'R' : return TargetLocation::RIGHT;
-        default  : return TargetLocation::INVALID;
-    }
-}
 
-typedef std::tuple<TargetLocation, TargetLocation, TargetLocation> FieldLayout;
+typedef std::tuple<TargetLocation, TargetLocation> FieldAutoLayout;
 
-FieldLayout
-parse_field_layout_message(const std::string &message = frc::DriverStation::GetInstance().GetGameSpecificMessage()) {
-    return (message.empty()) ?
-           std::make_tuple(TargetLocation::INVALID,
-                           TargetLocation::INVALID,
-                           TargetLocation::INVALID)
-                             : std::make_tuple(parse_TargetLocation(message.at(0)),
-                                               parse_TargetLocation(message.at(1)),
-                                               parse_TargetLocation(message.at(2)));
 
-}
+
+std::string to_string(StartPosition p);
+std::string to_string(TargetLocation l, bool use_top_bottom = true);
+std::string to_string(Alliance a);
+std::string to_string(FieldAutoLayout l);
+
+
+TargetLocation parse_TargetLocation(char c);
+FieldAutoLayout
+parse_field_layout_message(const std::string &message = frc::DriverStation::GetInstance().GetGameSpecificMessage());
 
 struct FieldModel {
 
     SendableChooser<Alliance> allianceChooser;
     SendableChooser<StartPosition> startChooser;
+    SendableChooser<AutoTarget> targetChooser;
 
 
-    std::string getFieldLayoutMessage(){
-        //TODO frc::DriverStation::GetGameSpecificMessage();
-        return SmartDashboard::GetString("FIELD","");
-    }
-    FieldLayout getFieldLayout(){return parse_field_layout_message(getFieldLayoutMessage());}
+    std::string getFieldLayoutMessage();
+    FieldAutoLayout getFieldLayout();
 
-    bool fieldLayoutIsValid(){return std::get<0>(getFieldLayout()) != TargetLocation::INVALID;}
+    bool fieldLayoutIsValid();
 
+    StartPosition getStartPosition();
 
-    FieldModel(){
-        allianceChooser.SetName("Robot","Alliance");
-        allianceChooser.AddDefault("red", Alliance::RED);
-        allianceChooser.AddObject("blue",Alliance::BLUE);
+    Alliance getAlliance();
 
-        startChooser.SetName("Robot","Start Position");
-        startChooser.AddDefault("bottom",StartPosition::BOTTOM);
-        startChooser.AddObject("middle",StartPosition::MIDDLE);
-        startChooser.AddObject("top",StartPosition::TOP);
+    FieldModel();
 
-        SmartDashboard::PutData(&allianceChooser);
-        SmartDashboard::PutData(&startChooser);
-
-        auto sd_table = nt::NetworkTableInstance::GetDefault().GetTable("SmartDashboard");
-
-        sd_table->GetSubTable(allianceChooser.GetName())
-                ->AddEntryListener("selected",
-                                   [](NetworkTable* ,
-                                      llvm::StringRef ,
-                                      nt::NetworkTableEntry,
-                                      std::shared_ptr<nt::Value> value,
-                                      int ){
-                                       llvm::outs() << "alliance chooser updated: "<<value->GetString() <<'\n';
-                                   },
-                                   nt::EntryListenerFlags::kUpdate);
-
-
-
-
-        SmartDashboard::SetDefaultString("FIELD","LRL");
-
-        sd_table->GetEntry("FIELD").AddListener([](const nt::EntryNotification& event){
-          auto value =  event.value->GetString();
-            llvm::outs() << "FIELD(ENTRY): "
-                         << value
-                         << "valid? = "
-                         <<  (std::get<0>(parse_field_layout_message(value))
-                              != TargetLocation::INVALID)
-                         << '\n';
-
-        },nt::EntryListenerFlags::kUpdate);
-
-        sd_table->AddEntryListener("FIELD",[](NetworkTable*,
-                                              llvm::StringRef,
-                                              nt::NetworkTableEntry,
-                                              std::shared_ptr<nt::Value> value,
-                                              int){
-                                       llvm::outs() << "FIELD: "
-                                                    << value->GetString()
-                                                    << "valid? = "
-                                                    <<  (std::get<0>(parse_field_layout_message(value->GetString()))
-                                                         != TargetLocation::INVALID)
-                                                    << '\n';
-                                   },
-                                   nt::EntryListenerFlags::kUpdate);
-    }
+    void update();
 
 };
