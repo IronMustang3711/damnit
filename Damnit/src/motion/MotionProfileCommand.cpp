52be3400bc0bf37b3cf17bc0b0f185a55db264bf
jasonnn <jasonmarkham@gmail.com>
2018-02-14 22:49:55 -0800

    I hope this is close to working.

--- Damnit/src/motion/MotionProfileCommand.cpp
+++ Damnit/src/motion/MotionProfileCommand.cpp
@@ -6,9 +6,137 @@
  */
 
 #include <motion/MotionProfileCommand.h>
+#include <Robot.h>
+#include "profile.h"
 
-MotionProfileCommand::MotionProfileCommand() {
-	// TODO Auto-generated constructor stub
+using namespace mp;
 
+//TODO: remove timeout once this is working.
+MotionProfileCommand::MotionProfileCommand() : frc::Command("Motion Profile", 5.0) {
+    auto c = Robot::chassis.get();
+    Requires(c);
+
+    left = RobotMap::chassisLeftFront.get();
+    right = RobotMap::chassisRightFront.get();
+
+    notifier = std::make_unique<Notifier>(&MotionProfileCommand::update, this);
+
+}
+
+void MotionProfileCommand::Initialize() {
+
+    it = PROFS.cbegin();
+
+
+    for (auto t : {left, right}) {
+        t->SelectProfileSlot(SLOT, TIMEOUT);
+
+        t->Config_kF(SLOT, 0.0, TIMEOUT);
+        t->Config_kP(SLOT, 0.0, TIMEOUT);
+        t->Config_kI(SLOT, 0.0, TIMEOUT);
+        t->Config_kD(SLOT, 0.0, TIMEOUT);
+
+//        t->Config_kF(SLOT, 0.8, TIMEOUT);
+//        t->Config_kP(SLOT, 20.0, TIMEOUT);
+//        t->Config_kI(SLOT, 0.1, TIMEOUT);
+//        t->Config_kD(SLOT, 30.0, TIMEOUT);
+
+        t->ConfigMotionProfileTrajectoryPeriod(0, TIMEOUT); //duration already set in profile array
+
+        //TODO am i going to use this?
+        t->SetStatusFramePeriod(StatusFrameEnhanced::Status_10_MotionMagic, 10, TIMEOUT);
+
+        t->GetSensorCollection().SetQuadraturePosition(0, TIMEOUT);
+        t->SetSelectedSensorPosition(0, SLOT, TIMEOUT);
+        //TODO: what is the difference between these 2?
+
+        t->Set(ControlMode::MotionProfile, SetValueMotionProfile::Disable);
+
+        t->ClearMotionProfileTrajectories();
+
+    }
+
+    fill(std::next(PROFS.cbegin(), 20));
+
+    notifier->StartPeriodic(0.05);
+}
+
+void MotionProfileCommand::Execute() {
+    update();
+    fill();
+    for (auto t : {left, right}) {
+
+        MotionProfileStatus status{};
+
+        t->GetMotionProfileStatus(status);
+
+        if (status.btmBufferCnt > 5) {
+            t->Set(ControlMode::MotionProfile, SetValueMotionProfile::Enable);
+        } else if (status.activePointValid && status.isLast) {
+            t->Set(ControlMode::MotionProfile, SetValueMotionProfile::Hold);
+            Cancel();
+        } else {
+            DriverStation::ReportError("something weird happening in MotionProfileCommand::Execute");
+        }
+
+        //TODO more diagnostic messages from status
+
+
+        SmartDashboard::PutNumber("error: " + t->GetName(),
+                                  t->GetClosedLoopError(SLOT));
+        SmartDashboard::PutNumber("target: " + t->GetName(),
+                                  t->GetClosedLoopTarget(SLOT));
+        SmartDashboard::PutNumber("position: " + t->GetName(),
+                                  t->GetSelectedSensorPosition(SLOT));
+        SmartDashboard::PutNumber("velocity: " + t->GetName(),
+                                  t->GetSelectedSensorVelocity(SLOT));
+
+        SmartDashboard::PutNumber("trajectory position: " + t->GetName(),
+                                  t->GetActiveTrajectoryPosition());
+        SmartDashboard::PutNumber("trajectory velocity: " + t->GetName(),
+                                  t->GetActiveTrajectoryVelocity());
+
+    }
+
+
+}
+
+bool MotionProfileCommand::IsFinished() {
+    return false; //we will cancel ourselves
+}
+
+void MotionProfileCommand::End() {
+    notifier->Stop();
+}
+
+void MotionProfileCommand::update() {
+    for (auto t : {left, right}) t->ProcessMotionProfileBuffer();
+
+}
+
+void MotionProfileCommand::fill(ProfIter stop) {
+    TrajectoryPoint leftPoint{};
+    TrajectoryPoint rightPoint{};
+
+    while (!left->IsMotionProfileTopLevelBufferFull()
+           && !right->IsMotionProfileTopLevelBufferFull()
+           && it != stop) {
+
+        leftPoint.zeroPos = rightPoint.zeroPos = (it == &PROFS.front());
+        leftPoint.isLastPoint = rightPoint.isLastPoint = (it == &PROFS.back());
+        leftPoint.timeDur = rightPoint.timeDur = TrajectoryDuration_10ms;
+
+        leftPoint.position = it->leftPosition;
+        leftPoint.velocity = it->leftVelocity / 600.0; //100 ms interval //TODO: do this during codegen
+        //TODO: also, is that correct?
+
+        rightPoint.position = it->rightPosition;
+        rightPoint.velocity = it->rightVelocity / 600.0;
+
+        left->PushMotionProfileTrajectory(leftPoint);
+        right->PushMotionProfileTrajectory(rightPoint);
+
+        it++;
+    }
 }
 
--- Damnit/src/motion/MotionProfileCommand.h
+++ Damnit/src/motion/MotionProfileCommand.h
@@ -6,13 +6,38 @@
  */
 
 #pragma once
+
 #include <WPILib.h>
 #include <ctre/Phoenix.h>
+#include "profile.h"
 
-class MotionProfileCommand  : public frc::Command{
+class MotionProfileCommand : public frc::Command {
 public:
-	MotionProfileCommand();
+    MotionProfileCommand();
+
+    static constexpr int SLOT = 0;
+    static constexpr int TIMEOUT = 10;
 
 protected:
+    void Initialize() override;
+
+    void Execute() override;
+
+    bool IsFinished() override;
+
+    void End() override;
+
+    void update();
+
+    typedef decltype(mp::PROFS)::const_iterator ProfIter;
+
+    void fill(ProfIter stopAt = mp::PROFS.cend());
+
+private:
+    std::unique_ptr<Notifier> notifier;
+    WPI_TalonSRX *left;
+    WPI_TalonSRX *right;
+
+    ProfIter it;
 };
 
