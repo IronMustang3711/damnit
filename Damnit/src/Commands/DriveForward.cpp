c163a16e62f60c40ff3dc8a2e9eec1e8b76e0581
jasonnn <jasonmarkham@gmail.com>
2018-02-15 09:24:26 -0800

    extra forward driving commands

--- Damnit/src/Commands/DriveForward.cpp
+++ Damnit/src/Commands/DriveForward.cpp
@@ -20,10 +20,10 @@ void DriveForward::Initialize() {
 
 bool DriveForward::IsFinished() {
 
-	if( Robot::chassis->driveStraightIsOnTarget()){
-		onTargetCount++;
-	}
-	return onTargetCount >= 10;
+    if (Robot::chassis->driveStraightIsOnTarget()) {
+        onTargetCount++;
+    }
+    return onTargetCount >= 10;
 }
 
 void DriveForward::End() {
@@ -37,5 +37,73 @@ DriveForward::DriveForward() : DriveForward(0.0) {
 }
 
 void DriveForward::Execute() {
-	Robot::chassis->mm_Periodic(distance);
+    Robot::chassis->mm_Periodic(distance);
 }
+
+/************* DumbDriveForward ***************/
+DumbDriveForward::DumbDriveForward(double time) : TimedCommand("Timed Drive Forward(" + std::to_string(time) + ")",
+                                                               time) {
+    Requires(Robot::chassis.get());
+}
+
+
+void DumbDriveForward::Execute() {
+    Robot::chassis->curvatureDrive(0.2, 0.0, false);
+}
+
+void DumbDriveForward::End() {
+    Command::End();
+}
+
+/************* DriveForwardGyroEncoder ***************/
+
+DriveForwardGyroEncoder::DriveForwardGyroEncoder(double distanceInches, double timeout)
+        : Command("Drive Forward(Encoder/Gyro): " + std::to_string(distanceInches), timeout), pid{  /*P*/     0.15,
+        /*I*/      0.0,
+        /*D*/      0.0,
+        /*F*/      0.01,
+        /*source*/ this,
+        /*output*/ this,
+        /*update rate*/ 0.02},
+          targetTicks{75.0 * distanceInches} {
+    Requires(Robot::chassis.get());
+}
+
+double DriveForwardGyroEncoder::encoderValue() {
+    return (RobotMap::chassisLeftFront->GetSensorCollection().GetQuadraturePosition()
+            + RobotMap::chassisRightFront->GetSensorCollection().GetQuadraturePosition()) / 2;
+}
+
+void DriveForwardGyroEncoder::PIDWrite(double output) {
+    double angleCorrection = (-1 / 45.0) * (RobotMap::ahrs->GetAngle() - initialHeading);
+    Robot::chassis->curvatureDrive(output, angleCorrection, false);
+}
+
+double DriveForwardGyroEncoder::PIDGet() {
+    return encoderValue() - initialEncoder;
+}
+
+void DriveForwardGyroEncoder::Initialize() {
+    Robot::chassis->resetEncoders();
+    initialEncoder = RobotMap::chassisLeftFront->GetSensorCollection().GetQuadraturePosition();
+    pid.SetInputRange(0, targetTicks);
+    pid.SetOutputRange(-0.3, 0.3);
+    pid.SetAbsoluteTolerance(0.1);
+    pid.SetSetpoint(targetTicks);
+    initialHeading = RobotMap::ahrs->GetAngle(); //Robot::chassis->getHeading();
+    pid.Enable();
+}
+
+void DriveForwardGyroEncoder::Execute() {
+    SmartDashboard::PutNumber("DriveStraight: error", pid.GetError());
+}
+
+void DriveForwardGyroEncoder::End() {
+    pid.Disable();
+    Robot::chassis->stop();
+}
+
+bool DriveForwardGyroEncoder::IsFinished() {
+    return IsTimedOut() || pid.OnTarget();
+}
+
--- Damnit/src/Commands/DriveForward.h
+++ Damnit/src/Commands/DriveForward.h
@@ -2,11 +2,15 @@
 // Created by Jason Markham on 2/13/18.
 //
 
-#ifndef DAMNIT_DRIVEFORWARD_H
-#define DAMNIT_DRIVEFORWARD_H
+#pragma once
 
 
 #include <Commands/Command.h>
+#include <Commands/TimedCommand.h>
+#include <PIDSource.h>
+#include <PIDOutput.h>
+#include <PIDController.h>
+
 
 class DriveForward : public frc::Command {
 public:
@@ -31,4 +35,53 @@ private:
 };
 
 
-#endif //DAMNIT_DRIVEFORWARD_H
+class DumbDriveForward : public frc::TimedCommand {
+public:
+    explicit DumbDriveForward(double time = 2.0);
+
+protected:
+    void Execute() override;
+
+    void End() override;
+
+
+};
+
+
+class DriveForwardGyroEncoder : public frc::Command,
+                                public frc::PIDSource,
+                                public frc::PIDOutput {
+public:
+    explicit DriveForwardGyroEncoder(double distanceInches = 48.0, double timeout = 0.0);
+
+    double encoderValue();
+
+
+// PIDOutput interface
+    void PIDWrite(double output) override;
+
+    // PIDSource interface
+    double PIDGet() override;
+
+
+protected:
+    void Initialize() override;
+
+    void Execute() override;
+
+    void End() override;
+
+    bool IsFinished() override;
+
+private:
+    PIDController pid;
+    double targetTicks = 0;
+    double initialHeading = 0;
+    double initialEncoder = 0;
+
+
+};
+
+
+
+
