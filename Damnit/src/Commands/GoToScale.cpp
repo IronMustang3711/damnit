dbe5b61d41839376055e2a1c6c5f83a7814bef72
jasonnn <jasonmarkham@gmail.com>
2018-02-27 15:44:24 -0800

    GOToScaleTweaks

--- Damnit/src/Commands/GoToScale.cpp
+++ Damnit/src/Commands/GoToScale.cpp
@@ -7,6 +7,17 @@ GoToScale::GoToScale() : frc::CommandGroup("Go To Scale") {
 	// stow clamp
 	AddSequential(new StowClamp);  // Hide clamp
 
-	AddParallel(new BucketTiltAutoLevel());//(new BucketTiltPosition(100));  // Put bucket in auto level
+	//TODO: this is probably wrong.
+	AddParallel(new BucketTiltAllTheWayBack());//(new BucketTiltPosition(100));  // Put bucket in auto level
 	AddParallel(new UpperTiltToScale());  // move arm to scale
+
+
+	//TODO: try this if the above sequence isnt working
+//	auto bucketTiltSequence = new CommandGroup();
+//	bucketTiltSequence->AddSequential(new BucketTiltAllTheWayBack,0.5);
+//	bucketTiltSequence->AddSequential(new BucketTiltAutoLevel);
+//
+//	AddSequential(new StowClamp);
+//	AddSequential(bucketTiltSequence);
+//	AddParallel(new UpperTiltToScale);
 }
