63e125392321b5a570322df4b1e6f808dde41bc5
jasonnn <jasonmarkham@gmail.com>
2018-04-05 11:03:31 -0700

    comment out closed loop error

--- Damnit/src/auto/MotionProfileExecutor.cpp
+++ Damnit/src/auto/MotionProfileExecutor.cpp
@@ -52,7 +52,7 @@ void MotionProfileExecutor::Initialize() {
         t->Config_kI(CLOSED_LOOP_SLOT, 0.0, TIMEOUT);
         t->Config_kD(CLOSED_LOOP_SLOT, 0.0, TIMEOUT); //10
 
-        t->ConfigAllowableClosedloopError(PRIMARY_PID_IDX,5,TIMEOUT);
+    //    t->ConfigAllowableClosedloopError(PRIMARY_PID_IDX,5,TIMEOUT);
 
         t->ConfigMotionProfileTrajectoryPeriod(0, TIMEOUT); //duration already set in profile array
 
